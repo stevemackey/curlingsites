ece0a8a2835d65c114f09e0c02597fa9a4adec29
Steve Mackey <s_mackey@live.com>
2021-01-29 04:15:55 -0700

    Commit Facebook

--- Startup.cs
+++ Startup.cs
@@ -68,6 +68,11 @@ namespace CurlingLeagueManager
                 options.SlidingExpiration = true;
             });
 
+            services.AddAuthentication().AddFacebook(facebookOptions =>
+            {
+                facebookOptions.AppId = Configuration["Authentication:Facebook:AppId"];
+                facebookOptions.AppSecret = Configuration["Authentication:Facebook:AppSecret"];
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
